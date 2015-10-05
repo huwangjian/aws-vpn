d98520ee1a0301243054b3120126b83eebe76935
huwangjian <huwangjian@users.noreply.github.com>
2015-10-05 13:33:39 +0800

    add shadowsocks-libev

---
+++ shadowsocks/ss-libev.sh
@@ -0,0 +1,5 @@
+# http://teddysun.com/357.html
+
+wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-libev.sh
+chmod +x shadowsocks-libev.sh
+./shadowsocks-libev.sh 2>&1 | tee shadowsocks-libev.log

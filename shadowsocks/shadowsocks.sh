af3aff0d8f37a154c8f4b49eb47ed01be6be3004
huwangjian <huwangjian@users.noreply.github.com>
2017-11-17 23:55:42 +0800

    shadowsocks-all.sh

--- shadowsocks/shadowsocks.sh
+++ shadowsocks/shadowsocks.sh
@@ -1,5 +1,5 @@
-# http://teddysun.com/358.html
+# https://teddysun.com/486.html
 
-wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-libev-debian.sh
-chmod +x shadowsocks-libev-debian.sh
-./shadowsocks-libev-debian.sh 2>&1 | tee shadowsocks-libev-debian.log
+wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
+chmod +x shadowsocks-all.sh
+./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
